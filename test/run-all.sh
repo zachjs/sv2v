9c1fc7d0206e13e5d80449f2751a381d278354db
Zachary Snow <zach@zachjs.com>
2019-04-18 00:58:58 -0400

    test runner surfaces failures

--- test/run-all.sh
+++ test/run-all.sh
@@ -2,10 +2,18 @@
 
 cd `dirname "${BASH_SOURCE[0]}"`
 
+failures=0
 for script in `ls */run.sh`; do
     suite=`dirname $script`
     echo "--------------------"
     echo "SUITE: $suite"
     echo "--------------------"
     (cd $suite; ./run.sh)
+    if [ $? -ne 0 ]; then
+        failures=`expr $failures + 1`
+    fi
 done
+if [ $failures -ne 0 ]; then
+    echo "$failures test suite(s) failed"
+    exit 1
+fi
