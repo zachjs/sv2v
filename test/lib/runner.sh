bafe7e43962a9fb17791294c09bbc0aacfe9a9eb
Zachary Snow <zach@zachjs.com>
2019-09-02 12:54:32 -0400

    test runner supports test selection

--- test/lib/runner.sh
+++ test/lib/runner.sh
@@ -2,10 +2,22 @@
 
 SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
 
+tests=`ls *.sv | sed -e "s_\.sv\\\$__"`
+
+if [ "$1" ]; then
+    tests="$1"
+    shift
+fi
+
+addTest() {
+    test="$1"
+    eval "test_$test() { runTest \"$test\"; }"
+    suite_addTest "test_$test"
+}
+
 suite() {
-    for test in `ls *.sv | sed -e "s_\.sv\\\$__"`; do
-        eval "test_$test() { runTest \"$test\"; }"
-        suite_addTest "test_$test"
+    for test in $tests; do
+        addTest $test
     done
 }
 
