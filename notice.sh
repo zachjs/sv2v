2266dd69d676132904998b559674f2a18b286525
Zachary Snow <zach@zachjs.com>
2019-12-22 14:43:01 -0500

    add dependency licenses to NOTICE

---
+++ notice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dependencies=`stack ls dependencies \
+    | sed -e 's/ /-/' \
+    | grep -v "^sv2v-[0-9\.]\+\$" \
+    | grep -v "^rts-1\.0\$" \
+    `
+
+for dependency in `echo "$dependencies"`; do
+    license_url="https://hackage.haskell.org/package/$dependency/src/LICENSE"
+
+    echo "================================================================================"
+    echo "Dependency: $dependency"
+    echo "================================================================================"
+    echo ""
+    curl "$license_url" 2> /dev/null | sed -e "s/^/  /"
+    echo ""
+done
