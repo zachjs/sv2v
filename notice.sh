ccc6f174af607acd7c668e6c8acba434a207068a
Zachary Snow <zach@zachjs.com>
2022-02-15 18:11:52 +0100

    drop trailing whitespace in notice file

--- notice.sh
+++ notice.sh
@@ -14,6 +14,6 @@ for dependency in `echo "$dependencies"`; do
     echo "Dependency: $dependency"
     echo "================================================================================"
     echo ""
-    curl "$license_url" 2> /dev/null | sed -e "s/^/  /"
+    curl "$license_url" 2> /dev/null | sed -e "s/^/  /" -e "s/ *$//"
     echo ""
 done
