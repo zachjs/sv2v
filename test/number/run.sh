abbcaae02cb8b2828032943c7569e5173303780f
Zachary Snow <zach@zachjs.com>
2022-04-04 12:26:15 -0600

    default to python3 for number gen.py

--- test/number/run.sh
+++ test/number/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-python gen.py 4 b 2 01xz > binary.sv
-python gen.py 2 o 8 01234567xz > octal.sv
-python gen.py 2 d 10 0123456789 > decimal.sv
-python gen.py 2 h 16 0123456789abcdefxz > hex.sv
+py=$(which python3 || which python)
+$py gen.py 4 b 2 01xz > binary.sv
+$py gen.py 2 o 8 01234567xz > octal.sv
+$py gen.py 2 d 10 0123456789 > decimal.sv
+$py gen.py 2 h 16 0123456789abcdefxz > hex.sv
 source ../lib/runner.sh
