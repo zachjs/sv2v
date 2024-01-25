31ffa439f70722048d684bc5652d632029175924
Ethan Sifferman <ethan@sifferman.dev>
2024-01-25 11:57:38 -0800

    cleaned up SystemTasks.hs

--- src/Convert/SystemTasks.hs
+++ src/Convert/SystemTasks.hs
@@ -14,34 +14,36 @@ convert =
     map $ traverseDescriptions $ traverseModuleItems $
     traverseStmts $ traverseNestedStmts convertStmt
 
+timeCall :: Expr
+timeCall = Call (Ident "$time") (Args [] [])
+
 convertStmt :: Stmt -> Stmt
 convertStmt (Subroutine (Ident "$info") args) =
     Block Seq "" [] [
-        (Subroutine (Ident "$write") (Args [(String "[%0t] Info: "), (Call (Ident "$time") (Args [] []))] [])),
+        (Subroutine (Ident "$write") (Args [(String "[%0t] Info: "), timeCall] [])),
         (Subroutine (Ident "$display") args)
     ]
 
 convertStmt (Subroutine (Ident "$warning") args) =
     Block Seq "" [] [
-        (Subroutine (Ident "$write") (Args [(String "[%0t] Warning: "), (Call (Ident "$time") (Args [] []))] [])),
+        (Subroutine (Ident "$write") (Args [(String "[%0t] Warning: "), timeCall] [])),
         (Subroutine (Ident "$display") args)
     ]
 
 convertStmt (Subroutine (Ident "$error") args) =
     Block Seq "" [] [
-        (Subroutine (Ident "$write") (Args [(String "[%0t] Error: "), (Call (Ident "$time") (Args [] []))] [])),
+        (Subroutine (Ident "$write") (Args [(String "[%0t] Error: "), timeCall] [])),
         (Subroutine (Ident "$display") args)
     ]
 
 convertStmt (Subroutine (Ident "$fatal") (Args [] [])) =
     Block Seq "" [] [
-        (Subroutine (Ident "$write") (Args [(String "[%0t] Fatal: "), (Call (Ident "$time") (Args [] []))] [])),
-        (Subroutine (Ident "$display") (Args [] [])),
+        (Subroutine (Ident "$display") (Args [(String "[%0t] Fatal:"), timeCall] [])),
         (Subroutine (Ident "$finish") (Args [] []))
     ]
-convertStmt (Subroutine (Ident "$fatal") (Args (finishArgs:displayArgs) [])) =
+convertStmt (Subroutine (Ident "$fatal") (Args (finishArgs:displayArgs) _)) =
     Block Seq "" [] [
-        (Subroutine (Ident "$write") (Args [(String "Fatal:")] [])),
+        (Subroutine (Ident "$write") (Args [(String "[%0t] Fatal: "), timeCall] [])),
         (Subroutine (Ident "$display") (Args displayArgs [])),
         (Subroutine (Ident "$finish") (Args [finishArgs] []))
     ]
