52eb839eb593c0c5b73e5703ca12a538ccd40443
Ethan Sifferman <ethan@sifferman.dev>
2024-01-30 01:23:25 -0800

    increased code reusability

--- test/core/elab_task.v
+++ test/core/elab_task.v
@@ -1,17 +1,17 @@
 module top;
     integer _sv2v_elaboration_fatal = -1;
-    initial $display("Elaboration Info: ");
+    initial $display("Elaboration Info:");
     initial $display("Elaboration Info: ", "%b", 1);
-    initial $display("Elaboration Warning: ");
+    initial $display("Elaboration Warning:");
     initial $display("Elaboration Warning: ", "%b", 2);
-    initial $display("Elaboration Error: ");
+    initial $display("Elaboration Error:");
     initial $display("Elaboration Error: ", "%b", 3);
     initial begin
-        $display("Elaboration Fatal: ");
+        $display("Elaboration Fatal:");
         _sv2v_elaboration_fatal = 0;
     end
     initial begin
-        $display("Elaboration Fatal: ");
+        $display("Elaboration Fatal:");
         _sv2v_elaboration_fatal = 0;
     end
     initial begin
--- test/core/system_tasks.v
+++ test/core/system_tasks.v
@@ -1,12 +1,12 @@
 module top;
     initial begin
-        $display("[%0t] Info: ", $time);
+        $display("[%0t] Info:", $time);
         $display("[%0t] Info: ", $time, "%b", 1);
-        $display("[%0t] Warning: ", $time);
+        $display("[%0t] Warning:", $time);
         $display("[%0t] Warning: ", $time, "%b", 2);
-        $display("[%0t] Error: ", $time);
+        $display("[%0t] Error:", $time);
         $display("[%0t] Error: ", $time, "%b", 3);
-        $display("[%0t] Fatal: ", $time);
+        $display("[%0t] Fatal:", $time);
         $finish;
     end
 endmodule
