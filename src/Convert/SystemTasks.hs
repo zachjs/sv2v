5ad4ce9d3d000f9c9505e289defe6f8ccdaa6d8e
Ethan Sifferman <ethan@sifferman.dev>
2024-01-29 20:52:39 -0800

    added single finish for all elaboration fatals

--- src/Convert/SystemTasks.hs
+++ src/Convert/SystemTasks.hs
@@ -10,7 +10,39 @@ import Convert.Traverse
 import Language.SystemVerilog.AST
 
 convert :: [AST] -> [AST]
-convert = map $ traverseDescriptions $ traverseModuleItems convertModuleItem
+convert = map $ traverseDescriptions traverseDescription
+
+elaborationFatalIdent :: Identifier
+elaborationFatalIdent = "_sv2v_elaboration_fatal"
+
+elaborationFatalCancelCode :: Expr
+elaborationFatalCancelCode = RawNum (-1)
+
+elaborationFatalDecl :: ModuleItem
+elaborationFatalDecl = MIPackageItem $ Decl $ Variable Local t elaborationFatalIdent [] elaborationFatalCancelCode
+    where t = IntegerAtom TInteger Unspecified
+
+elaborationFatalCheck :: ModuleItem
+elaborationFatalCheck = Initial (Block Seq "" [] [
+        zeroDelay,
+        If NoCheck checkCancelCode finishCall Null
+    ]) where
+        zeroDelay = Timing (Delay (RawNum 0)) Null
+        checkCancelCode = BinOpA Ne [] (Ident elaborationFatalIdent) elaborationFatalCancelCode
+        finishCall = Subroutine (Ident "$finish") (Args [(Ident elaborationFatalIdent)] [])
+
+traverseDescription :: Description -> Description
+traverseDescription (Part att ext kw lif name pts items) =
+    traverseModuleItems convertModuleItem $
+    Part att ext kw lif name pts $
+    if hasElaborationFatal
+        then elaborationFatalDecl : items ++ [elaborationFatalCheck]
+        else items
+    where
+        hasElaborationFatal = any isElabTaskFatal items
+        isElabTaskFatal (ElabTask SeverityFatal _) = True
+        isElabTaskFatal _ = False
+traverseDescription description = traverseModuleItems convertModuleItem description
 
 convertModuleItem :: ModuleItem -> ModuleItem
 
