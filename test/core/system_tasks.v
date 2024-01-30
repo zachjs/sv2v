1ccd1cd593db1b19738e6ef412e3ad53de28fc88
Ethan Sifferman <ethan@sifferman.dev>
2024-01-29 16:41:15 -0800

    removed unnecessary $writes

--- test/core/system_tasks.v
+++ test/core/system_tasks.v
@@ -1,24 +1,12 @@
 module top;
-	initial begin
-		$write("[%0t] Info: ", $time);
-		$display;
-		$write("[%0t] Info: ", $time);
-		$display("%b", 1);
-		$write("[%0t] Warning: ", $time);
-		$display;
-		$write("[%0t] Warning: ", $time);
-		$display("%b", 2);
-		$write("[%0t] Error: ", $time);
-		$display;
-		$write("[%0t] Error: ", $time);
-		$display("%b", 3);
-		$display("[%0t] Fatal:", $time);
-		$finish;
-		$write("[%0t] Fatal: ", $time);
-		$display;
-		$finish(0);
-		$write("[%0t] Fatal: ", $time);
-		$display("%b", 4);
-		$finish(0);
-	end
+    initial begin
+        $display("[%0t] Info: ", $time);
+        $display("[%0t] Info: ", $time, "%b", 1);
+        $display("[%0t] Warning: ", $time);
+        $display("[%0t] Warning: ", $time, "%b", 2);
+        $display("[%0t] Error: ", $time);
+        $display("[%0t] Error: ", $time, "%b", 3);
+        $display("[%0t] Fatal: ", $time);
+        $finish;
+    end
 endmodule
