7a7482c9645a967f350a9833b618f7a74ea4b8b8
Zachary Snow <zach@zachjs.com>
2024-04-15 00:26:57 -0400

    fix GitHub Actions setup
    - refresh GitHub Actions versions
    - manually install shUnit2 v2.1.8
    - more portable usage check
    - run-all.sh exits on ctrl-c

--- test/help/run.sh
+++ test/help/run.sh
@@ -5,14 +5,7 @@ test_help() {
     assertTrue "getting help should succeed" $result
     assertNotNull "stdout should not be empty" "$stdout"
     assertNull "stderr should be empty" "$stderr"
-    lines=`echo "$stdout" | wc -l`
-    usage=`echo "$stdout" | tail -n +3 | head -n $(expr $lines - 5)`
-    usage="\`\`\`
-$usage
-\`\`\`"
-    if [[ ! $(<../../README.md) = *"$usage"* ]]; then
-        fail "Did not find matching usage in README!"
-    fi
+    python3 check_usage.py
 }
 
 source ../lib/functions.sh
